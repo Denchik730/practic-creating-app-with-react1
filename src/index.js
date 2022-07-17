119eac607a78af4f35c7f680aa32415aa42391a7
Denis <gabbas.denn@mail.ru>
2022-07-17 09:20:39 +0300

    training with elements and components

--- src/index.js
+++ src/index.js
@@ -3,19 +3,10 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-const text = 'Hello World!';
-
-const elem = (
-  <div>
-      <h2>{text}</h2>
-      <input type="text" />
-      <button>CLick</button>
-  </div>
-);
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  elem
+  <App/>
 );
 
