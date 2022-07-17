d9894c55e4213b51fb7d33e197063401bda59af5
Denis <gabbas.denn@mail.ru>
2022-07-17 14:43:21 +0300

    property components

--- src/App.js
+++ src/App.js
@@ -1,55 +1,32 @@
-import {Component} from 'react';
+// import {Component, StrictMode} from 'react';
 import './App.css';
 
-const Header = () => {
-	return <h2>Hello World!</h2>
-}
-
-// const Field = () => {
-// 	const holder = 'Enter here'
-// 	const styleField = {
-// 		width: '300px'
-// 	};
-// 	return <input 
-// 			placeholder={holder} 
-// 			type='text' 
-// 			style={styleField}/>
-
+// function WhoAmI (props) {
+// 	return (
+// 		<div>
+// 			<h1>My name is {props.name}, surname - {props.surname}</h1>
+// 			<a href={props.link}>My profile</a>
+// 		</div>
+// 	)
 // }
-
-class Field extends Component {
-	render() {
-		const holder = 'Enter here'
-	const styleField = {
-		width: '300px'
-	};
-
-	return <input 
-			placeholder={holder} 
-			type='text' 
-			style={styleField}/>
-	}
+function WhoAmI ({name, surname, link}) {
+	return (
+		<div>
+			<h1>My name is {name()}, surname - {surname}</h1>
+			<a href={link}>My profile</a>
+		</div>
+	)
 }
 
-function Btn() {
-	const text = 'Log in';
-	const logged = false;
-
-	
-
-	// const res = () => {
-	//   return 'Log in';
-	// }
-	// const p = <p>Log in</p>
-	return <button>{logged ? 'Enter' : text}</button>
-}
 
 function App() {
 	return (
 		<div className="App">
-			<Header/>
-			<Field/>
-			<Btn/>
+			{/* <WhoAmI name={{firstName: "John"}} surname="Smith" link="facebook.com"/>
+			<WhoAmI name={{firstName: "Alex"}} surname="Shepard" link="facebook.com"/> */}
+			<WhoAmI name={() => {return 'John'}} surname="Smith" link="facebook.com"/>
+			<WhoAmI name={() => {return 'John'}} surname="Shepard" link="facebook.com"/>
+
 		</div>
 	);
 }
