538c783f2190a94ec062ce7f1395c7c9032e1a27
Denis <gabbas.denn@mail.ru>
2022-07-19 18:34:28 +0300

    component states

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-// import {Component, StrictMode} from 'react';
+import { Component } from 'react';
 import './App.css';
 
 // function WhoAmI (props) {
@@ -9,13 +9,33 @@ import './App.css';
 // 		</div>
 // 	)
 // }
-function WhoAmI ({name, surname, link}) {
-	return (
-		<div>
-			<h1>My name is {name()}, surname - {surname}</h1>
-			<a href={link}>My profile</a>
-		</div>
-	)
+class WhoAmI extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			years: 27, 
+			text: '+++'
+		}
+	}
+ 
+	nextYear = () => {
+		console.log('+++');
+		this.setState(state => ({
+			years: state.years + 1
+		}))
+	}
+
+
+	render() {
+		const {name, surname, link} = this.props;
+		return (
+			<div>
+				<button onClick={this.nextYear} >{this.state.text}</button>
+				<h1>My name is {name}, surname - {surname}, age - {this.state.years}</h1>
+				<a href={link}>My profile</a>
+			</div>
+		)
+	}
 }
 
 
@@ -24,8 +44,10 @@ function App() {
 		<div className="App">
 			{/* <WhoAmI name={{firstName: "John"}} surname="Smith" link="facebook.com"/>
 			<WhoAmI name={{firstName: "Alex"}} surname="Shepard" link="facebook.com"/> */}
-			<WhoAmI name={() => {return 'John'}} surname="Smith" link="facebook.com"/>
-			<WhoAmI name={() => {return 'John'}} surname="Shepard" link="facebook.com"/>
+			{/* <WhoAmI name={() => {return 'John'}} surname="Smith" link="facebook.com"/>
+			<WhoAmI name={() => {return 'John'}} surname="Shepard" link="facebook.com"/> */}
+			<WhoAmI name={'John'} surname="Smith" link="facebook.com"/>
+			<WhoAmI name={'John'} surname="Shepard" link="facebook.com"/>
 
 		</div>
 	);
--- src/index.js
+++ src/index.js
@@ -7,8 +7,8 @@ import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <StrictMode>
-    <App/>
-  </StrictMode>
+	<StrictMode>
+		<App/>
+	</StrictMode>
 );
 
